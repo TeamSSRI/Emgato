fb81bd956164f436654dc9dee27b68bd99e07d3c
2205267949 <56783609+2205267949@users.noreply.github.com>
2024-10-20 20:57:27 -0500

    updated on login and register page

--- src/client/components/LoginPage.js
+++ src/client/components/LoginPage.js
@@ -1,7 +1,51 @@
 import React from 'react';
 
+import mainLogo from '../images/logo.png'
+import '../../index.css';
+
+
 const LoginPage = () => {
-    return <h1>Login Page</h1>;
+    
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();  // Prevent default form submission behavior (page reload)
+        
+        const username = event.target.username.value;
+        const password = event.target.password.value;
+
+        // You can add your login logic here, such as making an API request
+        console.log('Login attempt:', { username, password });
+    };
+    
+    return (
+        <div className="login-page">
+            {/* Logo positioned above the form */}
+            <img src={mainLogo} alt="Logo" className="logo" />
+            <form className="login-form" onSubmit={handleLoginSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div className="button-group">
+                    {/* Use a normal anchor tag for navigation to /register */}
+                    <a href="/register" className="button register-button">
+                        Register
+                    </a>
+                    <input 
+                        type="submit" 
+                        value="Login" 
+                        className="button login-button" 
+                    />
+                </div>
+            </form>
+        </div>
+    );
+    
+    
+    
 };
 
 export default LoginPage;
--- src/client/components/RegisterPage.js
+++ src/client/components/RegisterPage.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import mainLogo from '../images/logo.png';
 
 const RegisterPage = () => {
-    return <h1>Register Page</h1>;
+    return (
+        <>
+            <div className="register-page-container">
+                <div className="register-content">
+                    <form className='register-form' action='http://localhost:8080/register' method='post'>
+                        <div className='logo-row'>
+                            {/* Wrap the logo in an anchor tag */}
+                            <a href="/login">
+                                <img className='logo-img' src={mainLogo} alt="logo" />
+                            </a>
+                        </div>
+                        <div>
+                            <label className="label" htmlFor="usrname">Username:</label>
+                            <input id="usrname" type='text' name='username' />
+                        </div>
+                        <div>
+                            <label className="label" htmlFor="psw">Password:</label>
+                            <input id="psw" type="password" name='password' />
+                        </div>
+
+                        <div>
+                            <input type='submit' value="Register" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className='vp-backdrop'>
+            </div>
+        </>
+    );
 };
 
 export default RegisterPage;
